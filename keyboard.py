b406ca616071bf9ab7ea03f3317ddd9d6134f09e
Patrick Mallery <patrick@patrickmallery.com>
2023-07-08 18:36:44 +0100

    add keybaord display to entry click and close on done button

--- keyboard.py
+++ keyboard.py
@@ -1,28 +1,39 @@
-from tkinter import *
+from customtkinter import *
 from PIL import ImageTk,Image
-from tkinter import font
 
-root = Tk()
+
+root = CTk()
 root.title('onscreen keyboard')
 root.geometry('800x480')
-bgImg = PhotoImage('1.jpg')
 root.configure(background="blue")
 
-MainFrame = Frame(root, bd=10, width=800, height= 160)
-MainFrame.pack(side="bottom")
-Row1Frame = Frame(MainFrame, width=800, height=40)
-Row1Frame.pack()
-Row2Frame = Frame(MainFrame, width=800, height=40)
-Row2Frame.pack()
-Row3Frame = Frame(MainFrame, width=800, height=40)
-Row3Frame.pack()
+MainFrame = CTkFrame(root, height= 160, fg_color="black", border_width=0)
+Row1Frame = CTkFrame(MainFrame, width=800, height=40, fg_color="black", border_width=0)
+Row2Frame = CTkFrame(MainFrame, width=800, height=40, fg_color="black", border_width=0)
+Row3Frame = CTkFrame(MainFrame, width=800, height=40, fg_color="black", border_width=0)
+
+def keyboard_frames():
+    MainFrame.pack(side="bottom", fill="x")
+    Row1Frame.pack()
+    Row2Frame.pack()
+    Row3Frame.pack()
+    for i in range(len(KEYS)):
+        for key, pos in KEYS[i]:
+            if i == 0:
+                CTkButton(Row1Frame, width=70, font=('Quicksand', 16),text=key, command=lambda key=key: press(key)).grid(row=i, column=pos, padx=5, pady=5)
+            if i == 1:
+                CTkButton(Row2Frame, width=70, font=('Quicksand', 16),text=key, command=lambda key=key: press(key)).grid(row=i, column=pos, padx=5, pady=5)
+            if i == 2:
+                CTkButton(Row3Frame, width=70, font=('Quicksand', 16),text=key, command=lambda key=key: press(key)).grid(row=i, column=pos, padx=5, pady=5)
 
 
 # text input display
 exp = " "
 input = StringVar()
-display = Entry(root, width=800, textvariable=input)
+display = CTkEntry(root, width=800, textvariable=input, insertontime=0)
 display.pack()
+display.bind("<Button-1>", lambda event: keyboard_frames())
+
 
 def press(letter):
     global exp
@@ -32,8 +43,12 @@ def press(letter):
         input.set(exp)
     elif str(letter) == "DONE":
         input.set("")
-        Label(root, text=exp).pack()
+        CTkLabel(root, text=exp).pack()
         exp = ""
+        MainFrame.pack_forget()
+        Row1Frame.pack_forget()
+        Row2Frame.pack_forget()
+        Row3Frame.pack_forget()
     else:
         exp = exp + str(letter)
         input.set(exp)
@@ -44,14 +59,7 @@ KEYS = [
     [("A", 0),("S", 1),("D", 2),("F", 3),("G", 4),("H", 5),("J", 6),("K", 7),("L", 8)],
     [("Z", 0),("X", 1),("C", 2),("V", 3),("B", 4),("N", 5),("M", 6), ("CLEAR", 7), ("DONE", 8)]
 ]
-for i in range(len(KEYS)):
-    for key, pos in KEYS[i]:
-        if i == 0:
-            Button(Row1Frame, width=5, text=key, command=lambda key=key: press(key)).grid(row=i, column=pos)
-        if i == 1:
-            Button(Row2Frame, width=5, text=key, command=lambda key=key: press(key)).grid(row=i, column=pos)
-        if i == 2:
-            Button(Row3Frame, width=5, text=key, command=lambda key=key: press(key)).grid(row=i, column=pos)
+
 root.mainloop()
 
 
