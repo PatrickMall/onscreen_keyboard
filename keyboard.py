354078ce573d0439da03dd4eac31b93be8acecfd
Patrick Mallery <patrick@patrickmallery.com>
2023-07-08 17:28:14 +0100

    add clear feature

--- keyboard.py
+++ keyboard.py
@@ -26,7 +26,11 @@ display.pack()
 
 def press(letter):
     global exp
-    if str(letter) == "DONE":
+    if str(letter) == "CLEAR":
+        newExp = exp[:-1]
+        exp = newExp
+        input.set(exp)
+    elif str(letter) == "DONE":
         input.set("")
         Label(root, text=exp).pack()
         exp = ""
@@ -38,7 +42,7 @@ def press(letter):
 KEYS = [
     [("Q", 0),("W", 1),("E", 2),("R", 3),("T", 4),("Y", 5),("U", 6),("I", 7),("O", 8),("P", 9)],
     [("A", 0),("S", 1),("D", 2),("F", 3),("G", 4),("H", 5),("J", 6),("K", 7),("L", 8)],
-    [("Z", 0),("X", 1),("C", 2),("V", 3),("B", 4),("N", 5),("M", 6), ("DONE", 7)]
+    [("Z", 0),("X", 1),("C", 2),("V", 3),("B", 4),("N", 5),("M", 6), ("CLEAR", 7), ("DONE", 8)]
 ]
 for i in range(len(KEYS)):
     for key, pos in KEYS[i]:
