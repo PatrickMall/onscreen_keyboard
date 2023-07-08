f6a2fc8f088d2eff1806d983d134a9c1c638339c
Patrick Mallery <patrick@patrickmallery.com>
2023-07-08 16:58:56 +0100

    build main keyboard and basic entry display

---
+++ keyboard.py
@@ -0,0 +1,48 @@
+from tkinter import *
+from PIL import ImageTk,Image
+from tkinter import font
+
+root = Tk()
+root.title('onscreen keyboard')
+root.geometry('800x480')
+bgImg = PhotoImage('1.jpg')
+root.configure(background="blue")
+
+MainFrame = Frame(root, bd=10, width=800, height= 160)
+MainFrame.pack(side="bottom")
+Row1Frame = Frame(MainFrame, width=800, height=40)
+Row1Frame.pack()
+Row2Frame = Frame(MainFrame, width=800, height=40)
+Row2Frame.pack()
+Row3Frame = Frame(MainFrame, width=800, height=40)
+Row3Frame.pack()
+
+
+# text input display
+exp = " "
+input = StringVar()
+display = Entry(root, width=800, textvariable=input)
+display.pack()
+
+def press(letter):
+    global exp
+    exp = exp + str(letter)
+    input.set(exp)
+
+# top row
+KEYS = [
+    [("Q", 0),("W", 1),("E", 2),("R", 3),("T", 4),("Y", 5),("U", 6),("I", 7),("O", 8),("P", 9)],
+    [("A", 0),("S", 1),("D", 2),("F", 3),("G", 4),("H", 5),("J", 6),("K", 7),("L", 8)],
+    [("Z", 0),("X", 1),("C", 2),("V", 3),("B", 4),("N", 5),("M", 6), ("DONE", 7)]
+]
+for i in range(len(KEYS)):
+    for key, pos in KEYS[i]:
+        if i == 0:
+            Button(Row1Frame, width=5, text=key, command=lambda key=key: press(key)).grid(row=i, column=pos)
+        if i == 1:
+            Button(Row2Frame, width=5, text=key, command=lambda key=key: press(key)).grid(row=i, column=pos)
+        if i == 2:
+            Button(Row3Frame, width=5, text=key, command=lambda key=key: press(key)).grid(row=i, column=pos)
+root.mainloop()
+
+
